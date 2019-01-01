8ae4a9288e11013acc452e9c416d78adc89190e6
AjaiGuvaliour <46262662+AjaiGuvaliour@users.noreply.github.com>
2019-01-01 23:33:01 +0530

    firstcommit

---
+++ helloworld/src/main/java/com/erp/controller/HelloController.java
@@ -0,0 +1,22 @@
+package com.erp.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@CrossOrigin(origins = "*")
+@RestController
+public class HelloController {
+    @GetMapping(produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+	 public Map<String, String> hello() {
+    	HashMap<String, String> map = new HashMap<>();
+        map.put("key", "value");
+        map.put("foo", "bar");
+        map.put("aa", "bb");
+        return map;
+	 }
+}
